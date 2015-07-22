44165140d9093a837f7e0a18e1b8fde58807680c
Tom Graham <tom@heyupdate.com>
2015-07-22 09:42:56 +0100

    Initial commit

---
+++ src/DependencyInjection/Configuration.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace Radish\RadishBundle\DependencyInjection;
+
+use Symfony\Component\Config\Definition\Builder\TreeBuilder;
+use Symfony\Component\Config\Definition\ConfigurationInterface;
+
+class Configuration implements ConfigurationInterface
+{
+    public function getConfigTreeBuilder()
+    {
+        $treeBuilder = new TreeBuilder();
+        $rootNode = $treeBuilder->root('radish');
+
+        $rootNode
+            ->children()
+                ->arrayNode('connection')
+                    ->children()
+                        ->scalarNode('host')->end()
+                        ->scalarNode('port')->end()
+                        ->scalarNode('user')->end()
+                        ->scalarNode('password')->end()
+                        ->scalarNode('vhost')->end()
+                    ->end()
+                ->end()
+                ->arrayNode('exchanges')
+                    ->requiresAtLeastOneElement()
+                    ->useAttributeAsKey('name')
+                    ->prototype('array')
+                        ->children()
+                            ->booleanNode('durable')->defaultTrue()->end()
+                            ->scalarNode('type')
+                                ->validate()
+                                ->ifNotInArray(array('direct', 'topic', 'fanout'))
+                                    ->thenInvalid('Invalid exchange type "%s"')
+                                ->end()
+                            ->end()
+                        ->end()
+                    ->end()
+                ->end()
+                ->arrayNode('queues')
+                    ->requiresAtLeastOneElement()
+                    ->useAttributeAsKey('name')
+                    ->prototype('array')
+                        ->children()
+                            ->booleanNode('durable')->defaultTrue()->end()
+                            ->scalarNode('exchange')->isRequired()->end()
+                            ->scalarNode('dead_letter_exchange')->end()
+                            ->scalarNode('routing_key')->isRequired()->end()
+                        ->end()
+                    ->end()
+                ->end()
+                ->arrayNode('producers')
+                    ->requiresAtLeastOneElement()
+                    ->useAttributeAsKey('name')
+                    ->prototype('array')
+                        ->children()
+                            ->scalarNode('exchange')->end()
+                        ->end()
+                    ->end()
+                ->end()
+                ->arrayNode('consumers')
+                    ->requiresAtLeastOneElement()
+                    ->useAttributeAsKey('name')
+                    ->prototype('array')
+                        ->validate()
+                            ->ifTrue(function ($v) {
+                                return isset($v['queues']) && !empty($v['queues']) && isset($v['queue']) && !empty($v['queue']);
+                            })
+                            ->thenInvalid('A consumer configuration can contain either "queue" or "queues", but not both.')
+                        ->end()
+                        ->children()
+                            ->scalarNode('queue')->end()
+                            ->scalarNode('worker')->end()
+                            ->arrayNode('queues')
+                                ->useAttributeAsKey('queue')
+                                ->prototype('array')
+                                    ->children()
+                                        ->scalarNode('queue')->end()
+                                        ->scalarNode('worker')->end()
+                                    ->end()
+                                ->end()
+                            ->end()
+                            ->arrayNode('middleware')
+                                ->prototype('variable')->end()
+                            ->end()
+                        ->end()
+                    ->end()
+                ->end()
+            ->end();
+
+        return $treeBuilder;
+    }
+}
