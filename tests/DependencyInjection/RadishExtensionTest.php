35b91885167516ece4a8eeed064ba55cd0e06998
Tom Graham <tom@heyupdate.com>
2017-01-04 09:21:33 +0000

    Add test for configuration

---
+++ tests/DependencyInjection/RadishExtensionTest.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace Radish\RadishBundle\DependencyInjection;
+
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+
+class RadishExtensionTest extends \PHPUnit_Framework_TestCase
+{
+    public function testDefaultConnectionConfiguration()
+    {
+        $container = $this->createContainer();
+        $this->loadConfig($container, []);
+
+        $this->assertHasService($container, 'radish.broker.connection');
+    }
+
+    public function testConnectionConfiguration()
+    {
+        $container = $this->createContainer();
+        $this->loadConfig($container, [
+            'connection' => [
+                'host' => '1.2.3.4',
+                'port' => 1337,
+                'login' => 'admin',
+                'password' => 'password',
+                'vhost' => '/',
+            ]
+        ]);
+
+        $this->assertHasService($container, 'radish.broker.connection');
+    }
+
+    private function assertHasService(ContainerBuilder $container, $id)
+    {
+        $this->assertTrue($container->hasDefinition($id) || $container->hasAlias($id), sprintf('The service %s should be defined.', $id));
+    }
+
+    private function assertNotHasService(ContainerBuilder $container, $id)
+    {
+        $this->assertFalse($container->hasDefinition($id) || $container->hasAlias($id), sprintf('The service %s should not be defined.', $id));
+    }
+
+    private function loadConfig(ContainerBuilder $container, array $config = [])
+    {
+        $extension = new RadishExtension();
+
+        $extension->load([$config], $container);
+    }
+
+    private function createContainer($debug = true)
+    {
+        $container = new ContainerBuilder();
+        $container->setParameter('kernel.debug', $debug);
+
+        return $container;
+    }
+}
